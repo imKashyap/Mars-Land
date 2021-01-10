8bed89851a635356a8e810c058eac2c3f516e171
Rahul Kashyap <rahulkashyap4041@gmail.com>
2021-01-10 13:14:13 +0530

    Test

---
+++ app/src/main/java/com/example/marsland/network/MarsProperty.kt
@@ -0,0 +1,12 @@
+package com.example.marsland.network
+
+import com.squareup.moshi.Json
+
+data class MarsProperty (
+    val id: String,
+    @Json(name = "img_src") val imgSrcUrl: String,
+    val type: String,
+    val price: Double){
+    val isRental
+        get():Boolean = type == "rent"
+}
